1584c23dc05fac45bceeccb3341f3e783f918527
Silvia Saverino <silviasave89@gmail.com>
2023-12-12 19:05:55 +0000

    comparing the tweet.uuid with tweetId to return the array targetTweetObj

--- script.js
+++ script.js
@@ -1,28 +1,37 @@
-import {tweetsData} from './data.js'
+import { tweetsData } from './data.js'
 
 const tweetBtn = document.getElementById('tweet-btn')
 const tweetInput = document.getElementById('tweet-input')
 
-tweetBtn.addEventListener('click', function(){
+tweetBtn.addEventListener('click', function () {
     console.log(tweetInput.value)
 })
 
-document.addEventListener('click', function(e){
-    if(e.target.dataset.reply){
+document.addEventListener('click', function (e) {
+    if (e.target.dataset.reply) {
         console.log('ciao')
     }
-    else if(e.target.dataset.like){
+    else if (e.target.dataset.like) {
         handleLikeClick(e.target.dataset.like)
     }
 })
 
-function handleLikeClick(tweetId){
-    console.log(tweetId)
+function handleLikeClick(tweetId) {
+    // we are checking if the uuid property from data.js file 
+    // is the same as the one stored in tweetId data obtained when clicking the icon
+    // the test will be run for every element in the array, to return either
+    // true or false
+    // if true: that element will be save to targetTweetObj
+    // if false: that object will be filtered out and won't be saved int targetTweetObj
+    const targetTweetObj = tweetsData.filter(function (tweet) {
+        return tweet.uuid === tweetId
+    })
+    console.log(targetTweetObj)
 }
 
-function getFeedHtml(){
+function getFeedHtml() {
     let feedHtml = ``
-    tweetsData.forEach(function(tweet){
+    tweetsData.forEach(function (tweet) {
         feedHtml += `
         <div class="tweet">
             <div class="tweet-inner">
@@ -50,12 +59,12 @@ function getFeedHtml(){
                 </div>            
             </div>
         </div>
-    `       
+    `
     })
     return feedHtml
 }
 
-function render(){
-    document.getElementById('feed').innerHTML= getFeedHtml()
+function render() {
+    document.getElementById('feed').innerHTML = getFeedHtml()
 }
 render()
