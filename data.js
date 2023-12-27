a40a86bb615644ffed5c922088d3885073be84ae
Silvia Saverino <silviasave89@gmail.com>
2023-12-27 13:26:30 +0000

    add function to handleReplyClick where we toggle the class 'hidden' through clicks

--- data.js
+++ data.js
@@ -1,16 +1,15 @@
-export const tweetsData = [   
+export const tweetsData = [
     {
-        handle: `@TrollBot66756542 💎`,
+        handle: `@SupremeCat 💎`,
         profilePic: `assets/images/cattroll.png`,
         likes: 27,
         retweets: 10,
-        tweetText: `Buy Bitcoin, ETH Make 💰💰💰 low low prices. 
-            Guaranteed return on investment. HMU DMs open!!`,
+        tweetText: `Scientists have confirmed that napping for 18 hours a day is the secret to world domination. 🌎😺💤 Prepare for the reign of the furballs!`,
         replies: [],
         isLiked: false,
         isRetweeted: false,
         uuid: '4b161eee-c0f5-4545-9c4b-8562944223ee',
-    },    
+    },
     {
         handle: `@Xena ✅`,
         profilePic: `assets/images/xena.png`,
@@ -18,12 +17,12 @@ export const tweetsData = [
         retweets: 234,
         tweetText: `I need volunteers for a one-way mission to Mars 🪐. No experience necessary🚀`,
         replies: [
-                  {
+            {
                 handle: `@TomCruise ✅`,
                 profilePic: `assets/images/tomcruise.png`,
                 tweetText: `Yes! Sign me up! 😎🛩`,
             },
-                  {
+            {
                 handle: `@ChuckNorris ✅`,
                 profilePic: `assets/images/chucknorris.png`,
                 tweetText: `I went last year😴`,
@@ -33,7 +32,7 @@ export const tweetsData = [
         isRetweeted: false,
         uuid: '3c23454ee-c0f5-9g9g-9c4b-77835tgs2',
     },
-        {
+    {
         handle: `@NoobCoder12`,
         profilePic: `assets/images/avatar.png`,
         likes: 10,
@@ -41,18 +40,18 @@ export const tweetsData = [
         tweetText: `Are you a coder if you only know HTML?`,
         replies: [
             {
-                handle: `@StackOverflower ☣️`,
-                profilePic: `assets/images/overflow.png`,
+                handle: `@Diva100 ☣️`,
+                profilePic: `assets/images/diva.png`,
                 tweetText: `No. Onviosuly not. Go get a job in McDonald's.`,
             },
             {
                 handle: `@YummyCoder64`,
                 profilePic: `assets/images/flamingo.png`,
-                tweetText: `You are wonderful just as you are! ❤️`,
+                tweetText: `You are wonderful just as you are! Keep on learning❤️`,
             },
         ],
         isLiked: false,
         isRetweeted: false,
         uuid: '8hy671sff-c0f5-4545-9c4b-1237gyys45',
-    },     
+    },
 ]
--- script.js
+++ script.js
@@ -12,6 +12,8 @@ document.addEventListener('click', function(e){
         handleLikeClick(e.target.dataset.like)   
     }else if(e.target.dataset.retweet){
         handleRetweetClick(e.target.dataset.retweet)   
+    }else if(e.target.dataset.reply){
+        handleReplyClick(e.target.dataset.reply)
     }
 })
 
@@ -53,6 +55,10 @@ function handleRetweetClick(tweetId){
     retweetTargetObj.isRetweeted = !retweetTargetObj.isRetweeted
     render()
     
+}
+
+function handleReplyClick(replyId){
+    document.getElementById(`replies-${replyId}`).classList.toggle('hidden')
 }
 
 function getFeedHtml() {
